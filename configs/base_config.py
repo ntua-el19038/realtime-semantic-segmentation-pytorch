c0ffa7471094e27788e5ebaa089f057698909b6a
ntua-el19038 <el19038@mail.ntua.gr>
2024-11-12 17:41:39 +0200

    config changes

--- configs/base_config.py
+++ configs/base_config.py
@@ -1,72 +1,74 @@
 class BaseConfig:
     def __init__(self,):
         # Dataset
-        self.dataset = 'prostate'
-        self.dataroot = './datasets/prostate/'
-        self.num_class = 3
-        self.ignore_index = 255
+        self.dataset = 'cardiac'
+        self.data_root = './datasets/vol/biomedic3/cc215/data/ACDC/preprocessed'
+        # self.data_root = './datasets/uavid'
+        self.num_class = 4
+        self.ignore_index = 0
 
         # Model
-        self.model = 'farseenet3'
+        self.model = 'farseenet4'
         self.encoder = None
         self.decoder = None
         self.encoder_weights = 'imagenet'
-        
+
         # Detail Head (For STDC)
         self.use_detail_head = False
         self.detail_thrs = 0.1
         self.detail_loss_coef = 1.0
         self.dice_loss_coef = 1.0
         self.bce_loss_coef = 1.0
-        
+
         # Training
-        self.total_epoch = 50
+        self.total_epoch = 20
         self.base_lr = 0.01
-        self.train_bs = 1     # For each GPU
+        self.train_bs = 1 # For each GPU
+        self.accumulate_grad_batches = 1
         self.use_aux = False
         self.aux_coef = None
-        
+
         # Validating
         self.val_bs = 3      # For each GPU
         self.begin_val_epoch = 0    # Epoch to start validation
         self.val_interval = 1   # Epoch interval between validation
-        
+
         # Testing
         self.is_testing = False
         self.test_bs = 1
-        self.test_data_folder = './datasets/cityscapes/leftImg8bit/test'
+        self.test_data_folder = './datasets/vol/biomedic3/cc215/data/ACDC/ACDC_artefacted/RandomBias'
         self.colormap = 'cityscapes'
         self.save_mask = True
         self.blend_prediction = True
         self.blend_alpha = 0.3
-        
+
         # Loss
         self.loss_type = 'ce'
         self.class_weights = None
         self.ohem_thrs = 0.7
-        
+
         # Scheduler
         self.lr_policy = 'cos_warmup'
         self.warmup_epochs = 3
-        
+
         # Optimizer
         self.optimizer_type = 'adamw'
         self.momentum = 0.9         # For SGD
         self.weight_decay = 1e-4    # For SGD
-        
+
         # Monitoring
         self.save_ckpt = True
-        self.save_dir = 'save/farseenet_mitb5_train_all-prostate'
+        self.save_dir = './save/farseenet4_bs8_ga1_cardiac_2'
         self.use_tb = True          # tensorboard
         self.tb_log_dir = None
         self.ckpt_name = None
-        
+
         # Training setting
         self.amp_training = False
         self.resume_training = True
         self.load_ckpt = True
-        self.load_ckpt_path = 'save/farseenet_mitb5_train_all-prostate/last.pth'
-        self.base_workers = 10
+        self.load_ckpt_path = './save/farseenet4_bs8_ga1_cardiac_2/last.pth'
+        self.base_workers = 5
         self.random_seed = 1
         self.use_ema = False
 
@@ -81,14 +83,14 @@ class BaseConfig:
         self.saturation = 0.0
         self.h_flip = 0.0
         self.v_flip = 0.0
-        
+
         # DDP
         self.synBN = True
-        
+
         # Knowledge Distillation
         self.kd_training = False
-        self.teacher_ckpt = './save/farseenet_mitb0-gta/last.pth'
-        self.teacher_model = 'farseenet3'
+        self.teacher_ckpt = './save/sqnet_mitb0-gta/last.pth'
+        self.teacher_model = 'sqnet'
         self.teacher_encoder = None
         self.teacher_decoder = None
         self.kd_loss_type = 'kl_div'
--- configs/my_config.py
+++ configs/my_config.py
@@ -5,16 +5,16 @@ class MyConfig(BaseConfig):
     def __init__(self,):
         super(MyConfig, self).__init__()
         # Dataset
-        self.dataset = 'prostate'
-        self.data_root = './datasets/prostate/'
-        self.num_class = 3
-        
+        self.dataset = 'cardiac'
+        self.data_root = './datasets/vol/biomedic3/cc215/data/ACDC/preprocessed'
+        # self.data_root = './datasets/uavid'
+
         # Model
-        self.model = 'farseenet3'
-        
+        self.model = 'farseenet4'
+
         # Training
-        self.total_epoch = 50
-        self.train_bs = 1
+        self.total_epoch = 20
+        self.train_bs = 8
         self.loss_type = 'ce'
         self.optimizer_type = 'adamw'
         self.logger_name = 'seg_trainer'
@@ -22,30 +22,30 @@ class MyConfig(BaseConfig):
 
         # Validating
         self.val_bs = 3
-        
+
         # Testing
         self.is_testing = False
         self.test_bs = 1
-        self.test_data_folder = './datasets/cityscapes/leftImg8bit/test'
-        self.load_ckpt_path =  'save/farseenet_mitb5_train_all-prostate/last.pth'
+        self.test_data_folder = './datasets/vol/biomedic3/cc215/data/ACDC/ACDC_artefacted/RandomBias'
+        self.load_ckpt_path =  './save/farseenet4_bs8_ga1_cardiac_2c/last.pth'
         self.save_mask = True
-        
+
         # Training setting
         self.use_ema = False
-        
+
         # Augmentation
         self.crop_size = 512
-        self.randscale = [-0.5, 1.0]
+        self.randscale = [0.5, 1.0]
         self.scale = 1.0
         self.brightness = 0.5
         self.contrast = 0.5
         self.saturation = 0.5
         self.h_flip = 0.5
-        
+
         # Knowledge Distillation
         self.kd_training = False
-        self.teacher_ckpt = 'save/farseenet_mitb5_train_all-prostate/last.pth'
-        self.teacher_model = 'farseenet3'
+        self.teacher_ckpt = './save/sqnet_mitb5_train_all-synthia_gta/last.pth'
+        self.teacher_model = 'sqnet'
         self.teacher_encoder = None
         self.teacher_decoder = None
 
